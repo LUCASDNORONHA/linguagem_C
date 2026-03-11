cf902249a456cd9a3298c585a5661e75054ad4c8
Lucas Dias Noronha <lucasdiasnoronha1@gmail.com>
2026-03-11 00:10:48 -0300

    Update

--- Aula 01/exercicio02_aula1.c
+++ Aula 01/exercicio02_aula1.c
@@ -31,7 +31,8 @@ int main(void) {
 	
 	for(int i = 0; i  < 3; i++) {
 		for(int j = 0; j < 3; j++) {
-			printf("Matriz[%d][%d] = %d \n", i, j, matrix[i][j]);
+			printf("[%d]", matrix[i][j]);
 		}
+		printf("\n");
 	}
 }
